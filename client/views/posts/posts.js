afa750f2d647165c9a89c1929d271521f806724a
ccorcos <ccorcos@gmail.com>
2014-05-28 15:12:35 -0700

    multiple posts

--- client/views/posts/posts.js
+++ client/views/posts/posts.js
@@ -1,3 +1,21 @@
 Template.posts.posts = function() {
     return Posts.find()
 }
+
+var defaultPost = {
+    text: "#New Post#\n\n"
+}
+
+Template.posts.events({
+    'click .new.post': function(event, template) {
+        postId = Posts.insert(defaultPost)
+        Router.go('editor', {
+            _id: postId
+        })
+    },
+    'click .delete.post': function(event, template) {
+        if (confirm("Are you sure you want to delete this post?")) {
+            Posts.remove(this._id)
+        }
+    }
+})
