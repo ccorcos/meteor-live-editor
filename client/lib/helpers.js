f80ef6bfc0b8c8e5dcffa48b111e41492227f273
ccorcos <ccorcos@gmail.com>
2014-05-29 11:12:00 -0700

    inserting into markdown working

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -17,3 +17,12 @@ UI.registerHelper('markdownTitle', function(text) {
     title = text.split('\n')[0].replace(/#/g, '')
     return title ? title : "Untitled"
 });
+
+UI.registerHelper('dateFromUTC', function(utc) {
+    td = new Date(utc);
+    dd = td.getDate();
+    monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    mm = monthNames[td.getMonth()]
+    yyyy = td.getFullYear();
+    return mm + ' ' + dd + ' ' + yyyy;
+})
--- client/views/editor/editor.js
+++ client/views/editor/editor.js
@@ -1,9 +1,49 @@
+getEditor = function() {
+    return $('#liveEditor')
+}
+
+updateEditor = function(id) {
+    textarea = getEditor()[0]
+    Posts.update(id, {
+        $set: {
+            text: textarea.value
+        }
+    })
+}
+
 Template.editor.events({
     'keyup #liveEditor': function(event, template) {
-        Posts.update(this._id, {
-            $set: {
-                text: event.target.value
-            }
-        })
+        updateEditor(this._id)
     }
 })
+
+Template.selectFile.events({
+    'click .select.file': function(event, template) {
+        $('#insertFile').modal('hide')
+        if (this.type().split('/')[0] === 'image') {
+            string = '![' + this.name() + '](' + this.url().split('?')[0] + ')'
+            insertText(string)
+        } else {
+            string = '[' + this.name() + '](' + this.url().split('?')[0] + ')'
+            insertText(string)
+        }
+    }
+})
+
+Template.selectFile.uploads = function() {
+    return Uploads.find({}, {
+        sort: {
+            'metadata.date': -1
+        }
+    })
+}
+
+insertText = function(string) {
+    textarea = getEditor()
+    text = textarea.val()
+    range = textarea.textrange('get')
+    textBefore = text.substring(0, range.start),
+    textMiddle = text.substring(range.start, range.end)
+    textAfter = text.substring(range.end, text.length);
+    textarea.val(textBefore + string + textMiddle + textAfter);
+}
