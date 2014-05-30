811dcdbb774c080b3eac0736a4a939532e224bf0
ccorcos <ccorcos@gmail.com>
2014-05-29 17:43:01 -0700

    working

--- models/models.js
+++ models/models.js
@@ -1,17 +1,5 @@
 Posts = new Meteor.Collection("posts")
 
-// var base = "";
-
-// if (Meteor.isServer) {
-//     base = process.env.PWD;
-// }
-
-// Uploads = new FS.Collection("uploads", {
-//     stores: [new FS.Store.FileSystem("uploads", {
-//         path: base + "/uploads"
-//     })]
-// });
-
 var uploadStore = new FS.Store.GridFS("uploads");
 
 Uploads = new FS.Collection("uploads", {
